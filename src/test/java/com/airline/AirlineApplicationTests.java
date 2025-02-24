35813739446443192d1d55e2110aa2ed9f4dfdf6
Rajmate16 <97882958+Rajmate16@users.noreply.github.com>
2025-02-24 17:49:57 +0530

    Update AirlineApplicationTests.java

--- src/test/java/com/airline/AirlineApplicationTests.java
+++ src/test/java/com/airline/AirlineApplicationTests.java
@@ -1,13 +1,13 @@
-package com.airline;
+// package com.airline;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+// import org.junit.jupiter.api.Test;
+// import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class AirlineApplicationTests {
+// @SpringBootTest
+// class AirlineApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+// 	@Test
+// 	void contextLoads() {
+// 	}
 
-}
+// }
